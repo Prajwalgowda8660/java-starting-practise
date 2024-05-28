f532a343f0e205020de887a45fca2c4c9d5e4372
Prajwal Gowda H G <169121568+Prajwalgowda8660@users.noreply.github.com>
2024-05-28 18:58:39 +0530

    Create if else.java

---
+++ if else.java
@@ -0,0 +1,13 @@
+package info;
+
+public class Bro {
+public static void main(String[] args) {
+	int x=15;
+	if(x>10) {
+	System.out.println("hii");
+	}
+	else {
+		System.out.println("bye");
+	}
+}
+}
