29c32005cedf5202a0823d058322413d65656831
Prajwal Gowda H G <169121568+Prajwalgowda8660@users.noreply.github.com>
2024-05-28 19:39:35 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,34 @@
+package info;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int x=6;
+		switch(x) {
+		case 1:
+			System.out.println("sunday");
+			break;
+		case 2:
+			System.out.println("mon");
+			break;
+
+		case 3:
+			System.out.println("tue");
+			break;
+		case 4:
+			System.out.println("wed");
+			break;
+		case 5:
+			System.out.println("thru");
+			break;
+		case 6:
+			System.out.println("fri");
+			break;
+		case 7:
+			System.out.println("sat");
+			break;
+
+
+		}
+	}
+}
