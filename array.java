60122b5f9c0db00e1c68584d424d6b13443f067f
Prajwal Gowda H G <169121568+Prajwalgowda8660@users.noreply.github.com>
2024-06-12 19:00:44 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,16 @@
+package info;
+
+public class Array {
+	public static void main(String[] args) {
+		int num []= new int[4];
+		num[0]=12;
+		num[1]=10;
+		num[2]=13;
+		num[3]=16;
+		for(int i=0;i<=3;i++) {
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
